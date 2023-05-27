535b31e184904a1398a5e6cb3b6cc5371b3c137f
Woogie Kim <mdir2kor@gmail.com>
2023-05-27 13:00:11 +0900

    first init

---
+++ api/src/test/java/com/woogie/todo/api/TodoApiApplicationTest.java
@@ -0,0 +1,12 @@
+package com.woogie.todo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
